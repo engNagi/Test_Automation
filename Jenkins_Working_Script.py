9cad1aa93d9422fa2b543c0697bae60fbb58b4d7
mohamednagi <eng.m.nagi@gmail.com>
2018-12-06 18:50:07 +0100

    Testing Automation -Optimized script - Reading file from user through browsing - Running Diffkit on the uploaded files

--- Jenkins_Working_Script.py
+++ Jenkins_Working_Script.py
@@ -1,13 +1,14 @@
-#importing Libraries
+# importing Libraries
 import os
 import datetime
 import shutil
 from subprocess import *
 
 
-#JavaWrapperFunction
+# functions
+# JavaWrapperFunction
 def jarWrapper(*args):
-    process = Popen(['java', '-jar']+list(args), stdout=PIPE, stderr=PIPE)
+    process = Popen(['java', '-jar'] + list(args), stdout=PIPE, stderr=PIPE)
     ret = []
     while process.poll() is None:
         line = process.stdout.readline()
@@ -17,41 +18,35 @@ def jarWrapper(*args):
     ret += stdout.split('\n')
     if stderr != '':
         ret += stderr.split('\n')
-    for rets in ret:
-        print rets
     ret.remove('')
     return ret
 
-#File Parameters
-print"this is the uploaded file " + os.environ['Test.Plan.xml']
-print"this is the uploaded file " + os.environ['Test.lhs.csv']
-print"this is the uploaded file " + os.environ['Test.rhs.csv']
 
-#print"this is the Jenkins BUILD_TAG" + os.environ['BUILD_TAG']
-#x = os.listdir('/var/lib/jenkins/workspace/Local_Diffkit/diffkit-app.jar')
-#print x
+# create new directory
+def create_directory():
+    build = os.environ['BUILD_TAG']
+    date = datetime.datetime.now().strftime("%d-%m-%Y_%H:%M")
+    timestamp_folder = build + date
+    os.mkdir(timestamp_folder)
+    return timestamp_folder
 
-currentDir = os.getcwd()
-print "this is the current working Dir" + currentDir
 
-desDir = os.environ['BUILD_TAG'] + datetime.datetime.now().strftime("%d-%m-%Y_%H:%M")
-print "Making directory " + desDir
-os.mkdir(desDir)
+# Copyfile
+def copy_files(source, destination_directory, destination):
+    shutil.move(source, destination_directory + '/' + destination)
 
-shutil.move('Test.Plan.xml', desDir + '/'+ os.environ['Test.Plan.xml'])
-print 'hello'
-shutil.move('Test.lhs.csv', desDir + '/' + os.environ['Test.lhs.csv'])
-print 'hello'
-shutil.move('Test.rhs.csv', desDir + '/' + os.environ['Test.rhs.csv'])
-print 'hello'
 
-os.chdir(desDir)
+destination_directory = create_directory()
+print destination_directory
 
-afterDirchange = os.getcwd()
-print "this is the current working Dir" + afterDirchange
+# Copying files
+copy_files('Test.Plan.xml', destination_directory, os.environ['Test.Plan.xml'])
+copy_files('Test.lhs.csv', destination_directory, os.environ['Test.lhs.csv'])
+copy_files('Test.rhs.csv', destination_directory, os.environ['Test.rhs.csv'])
 
-# Any number of args to be passed to the jar file
+os.chdir(destination_directory)
 
+# Diffkit.jar arguments
 args = ['../diffkit-app.jar', '-planfiles', os.environ['Test.Plan.xml']]
 
 # Running Difkit
@@ -60,10 +55,3 @@ result = jarWrapper(*args)
 # Print the output
 for rest in result:
     print rest
-
-y = os.path.join(os.getcwd())
-x = os.listdir(y)
-
-
-
-
